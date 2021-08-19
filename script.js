d445f257f781f8bd62bdfb24b5b9368e6930df76
tanishapandey00 <tanishapandey00@gamil.com>
2021-08-19 12:43:35 +0530

    commiting this file

---
+++ script.js
@@ -0,0 +1,5 @@
+var sidebar=document.querySelector(".sidebar");
+var sidebarview=document.querrySelector(".sidebar-view");
+sidebarview.onclick = function(){
+    sidebar.classList.toggle("sidebar-view-mobile");
+}
